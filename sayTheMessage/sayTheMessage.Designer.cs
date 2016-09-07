aa7d804174e5fb5507a1755dc3860a8a246883d7
Matthew McDaid Groves <matthewmgroves@gmail.com>
2016-09-07 14:28:47 -0500

    third commit

--- sayTheMessage/sayTheMessage.Designer.cs
+++ sayTheMessage/sayTheMessage.Designer.cs
@@ -34,7 +34,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(122, 81);
-            this.button1.Name = "button1";
+            this.button1.Name = "we the people";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "we the people";
